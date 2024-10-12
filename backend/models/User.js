384a6398138a106ef802cf0a12d4f83fb797dfc0
Ajoukov <aapteryx@gmail.com>
2024-10-12 14:25:50 -0400

    dashboard

--- backend/models/User.js
+++ backend/models/User.js
@@ -3,37 +3,19 @@ const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
 
 const userSchema = new mongoose.Schema({
-  username: {
-    type: String,
-    required: true,
-    unique: true,
+  username: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  rating: { type: Number, default: 5 }, // Initial rating is 5 stars
+  level: { type: Number, default: 1 },  // Initial level is 1
+  settings: {
+    emailNotifications: { type: Boolean, default: true },
+    showAccessibility: { type: Boolean, default: false },
   },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  password: {
-    type: String,
-    required: true,
-  },
-});
+}, { timestamps: true });
 
-// Encrypt the password before saving
-userSchema.pre('save', async function (next) {
-  if (!this.isModified('password')) {
-    return next();
-  }
-  const salt = await bcrypt.genSalt(10);
-  this.password = await bcrypt.hash(this.password, salt);
-  next();
-});
-
-// Compare password for login
 userSchema.methods.matchPassword = async function (enteredPassword) {
   return await bcrypt.compare(enteredPassword, this.password);
 };
 
-const User = mongoose.model('User', userSchema);
-
-module.exports = User;
+module.exports = mongoose.model('User', userSchema);
